bb87b986a91586ad420cf238298b9b642342fd81
Vikram Waradpande <f2015454@pilani.bits-pilani.ac.in>
2017-02-20 01:04:24 +0530

    Added the files

---
+++ script.js
@@ -0,0 +1,49 @@
+
+var num1;
+
+function execute(){
+$('.container').remove();
+var num = document.forms[0].elements[0].value;
+var container = document.createElement('div');
+num1=num;
+container.className = 'container';
+document.body.appendChild(container);
+$('.container').css("height", Math.sqrt(num)*7+'px');
+
+$('.container').css("width",Math.sqrt(num)*7 + 'px');
+
+draw();
+}
+
+function draw(){
+
+for(var i=1;i<=num1;i++){
+	var newid = document.createElement('div');
+newid.className = 'small-box';
+newid.id = i;
+if(isPrime(i)){
+	newid.className = 'small-box-red';
+  }
+document.querySelector(".container").appendChild(newid);	
+}
+
+}
+
+function isPrime(i){
+	if (i==1)
+		return false;
+	for(var j=2;j<=i/2;j++){
+      
+ 
+        if(i==2 || i==3 || i==5)
+        	{return true;}
+
+         if(i%j==0){
+         	return false;
+         }
+      
+
+	}
+	return true;
+}
+
